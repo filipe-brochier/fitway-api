c97c49562a317a15327c192ef5e5a0dfec305ed2
Filipe Brochier <filipe@clae.com.br>
2025-04-04 09:52:45 -0300

    refactor: use dayjs in prisma check ins repository

--- src/repositories/prisma/prisma-check-ins-repository.ts
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/prisma'
 import { CheckIn, Prisma } from '@prisma/client'
 import { CheckInsRepository } from '../check-ins-repository'
+import dayjs from 'dayjs'
 
 export class PrismaCheckInsRepository implements CheckInsRepository {
   async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
@@ -15,12 +16,15 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
     userId: string,
     date: Date,
   ): Promise<CheckIn | null> {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
     const checkIn = await prisma.checkIn.findFirst({
       where: {
         user_id: userId,
         created_at: {
-          gte: date,
-          lt: new Date(date.getTime() + 24 * 60 * 60 * 1000),
+          gte: startOfTheDay.toDate(),
+          lte: endOfTheDay.toDate(),
         },
       },
     })
