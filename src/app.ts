e7b972cfa336faa3c43ef9bd1e7ae4da504e3d26
Filipe Brochier <filipe@clae.com.br>
2025-04-04 13:48:10 -0300

    feat: created check-ins routes and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
+
+import { env } from './env'
+import { ZodError } from 'zod'
+
 import { usersRoutes } from './http/controllers/users/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
-import { ZodError } from 'zod'
-import { env } from './env'
-import fastifyJwt from '@fastify/jwt'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
@@ -13,6 +16,7 @@ app.register(fastifyJwt, {
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
--- src/use-cases/factories/make-validate-check-in-use-case.ts
+++ src/use-cases/factories/make-validate-check-in-use-case.ts
@@ -1,7 +1,7 @@
 import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
 import { ValidateCheckInUseCase } from '@/use-cases/validate-check-in'
 
-export function makeValidateCheckInHistoryUseCase() {
+export function makeValidateCheckInUseCase() {
   const checkInsRepository = new PrismaCheckInsRepository()
   const useCase = new ValidateCheckInUseCase(checkInsRepository)
 
